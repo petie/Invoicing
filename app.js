e87cc79eb76891458f1a9229650e0bb9f8befcf1
Piotr Korgol <piotr.korgol@gmail.com>
2017-07-31 13:52:59 +0200

    Added database

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
-
+const models = require('./models')
 const app = express();
 
 require('./config/express')(app)
 require('./config/routes')(app)
+models.sequelize.sync().then(() => {
+  app.listen(3000, () => {
+    console.log('Startup app on port 3000');
+  });
+})
 
-app.listen(3000, () => {
-  console.log('Startup app on port 3000');
-});
 
 module.exports = {
   app
